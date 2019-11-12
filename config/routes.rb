7178db80bea5bdbf62a40648ec20836c59f8c8a6
Eric <Aerelioyanadel@gmail.com>
2019-11-12 14:58:51 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'tasks#index'              # TasksController, index action
-  post '/tasks' => 'tasks#create' # TasksController, create action
+  root 'tasks#index'                     # TasksController, index action
+  post '/tasks'       => 'tasks#create'  # TasksController, create action
+  delete '/tasks/:id' => 'tasks#destroy' # TasksController, destroy action                 #
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
