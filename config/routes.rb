c1ca64e1500268587288e6e373ad02fb7f095c45
Chuniversity <contact@chunis.org>
2020-02-02 21:37:13 -0500

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'tasks#index' # TasksController, index action
   resources :tasks, only: [:create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
